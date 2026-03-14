bd8e76cdda9540a981ad035310a0a2a2cc80090c
Om Sathe <om.sathe@ontariotechu.net>
2026-03-14 19:30:35 -0400

    Fixed games tab
    added combo and battles page to fix since typescript does not like running when things shouldnt work

---
+++ my-app/app/games/combos/page.tsx
@@ -0,0 +1,16 @@
+import Navbar from "../../../components/Navbar";
+
+export default function CombosPage() {
+  return (
+    <div className="min-h-screen bg-white text-[#111827]">
+      <Navbar active="Games" />
+
+      <main className="w-full max-w-6xl mx-auto mt-8 px-4 pb-12">
+        <h1 className="text-4xl font-bold text-black">Sign Combos</h1>
+        <p className="mt-2 text-lg text-gray-600">
+          Combine signs to make phrases and sentences.
+        </p>
+      </main>
+    </div>
+  );
+}
